a8c14017ee91d0bcc0e963b60cd09b6e60f5108c
Abhyudit Jain <abhyuditjain@gmail.com>
2023-02-05 23:45:32 +0530

    changed initial layer

--- session6/model.py
+++ session6/model.py
@@ -15,13 +15,13 @@ class Net(nn.Module):
 
         CONVOLUTION BLOCK 1
         NIn    RFIn   KernelSize  Padding    Stride  JumpIn  JumpOut   RFOut     NOut
-        32      1       3           1           1       1       1        3        32
-        32      3       3           1           1       1       1        5        32
+        32      1       5           2           1       1       1        5        32
         32      5       3           1           1       1       1        7        32
         32      7       3           1           1       1       1        9        32
+        32      9       3           1           1       1       1       11        32
         """
         self.conv_block1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(32),
             nn.Dropout(dropout_value),
@@ -52,10 +52,10 @@ class Net(nn.Module):
 
         CONVOLUTION BLOCK 2
         NIn    RFIn   KernelSize  Padding   Stride    JumpIn  JumpOut    RFOut     NOut
-        32       9      3+2          1         2        1        2        13        15
-        15      13       3           1         1        2        2        17        15
-        15      17       3           1         1        2        2        21        15
-        15      21       3           1         1        2        2        25        15
+        32      11      3+2          1         2        1        2        15        15
+        15      15       3           1         1        2        2        19        15
+        15      19       3           1         1        2        2        23        15
+        15      23       3           1         1        2        2        27        15
         """
         self.conv_block2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, dilation=2, bias=False),
@@ -89,10 +89,10 @@ class Net(nn.Module):
 
         CONVOLUTION BLOCK 3
         NIn    RFIn   KernelSize  Padding   Stride    JumpIn  JumpOut    RFOut     NOut
-        15      25      3+2          1         2         2       4        33         7
-         7      33       3           1         1         4       4        41         7
-         7      41       3           1         1         4       4        49         7
-         7      49       3           1         1         4       4        57         7
+        15      27      3+2          1         2         2       4        35         7
+         7      35       3           1         1         4       4        43         7
+         7      43       3           1         1         4       4        51         7
+         7      51       3           1         1         4       4        59         7
         """
         self.conv_block3 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, dilation=2, bias=False),
@@ -126,19 +126,12 @@ class Net(nn.Module):
 
         CONVOLUTION BLOCK 4
         NIn    RFIn   KernelSize  Padding   Stride    JumpIn  JumpOut    RFOut     NOut
-         7      57      3+2          1        2         4        8        73        3
-         3      73       3           1        1         8        8        89        3
-         3      89       3           1        1         8        8        105       3
-         3      105      3           0        1         8        8        121       1
+         7      59      3+2          1        2         4        8        75        3
+         3      75       3           1        1         8        8        91        3
+         3      91       3           1        1         8        8        107       3
         """
         self.conv_block4 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, dilation=2, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Dropout(dropout_value),
-
-            nn.Conv2d(in_channels=32, out_channels=64, groups=32, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=1, stride=1, padding=0, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(32),
             nn.Dropout(dropout_value),
